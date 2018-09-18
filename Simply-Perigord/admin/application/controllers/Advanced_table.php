c9310a2e2c80079b185f761617690f1a2c96d02f
Debashis Nath <debashisnath1992@gmail.com>
2018-09-18 18:44:28 +0530

    admin panel

---
+++ Simply-Perigord/admin/application/controllers/Advanced_table.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Advanced_table extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('advanced_table');
+	}
+
+}
+
+/* End of file Advanced_table.php */
+/* Location: ./application/controllers/Advanced_table.php */
