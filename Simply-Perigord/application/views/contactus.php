d6a9637c5aedf774b0ea77f709f09b625ce432d2
Deep Bratt Mondal <deepbratt@gmail.com>
2018-09-21 09:54:33 +0530

    ssssssssad

---
+++ Simply-Perigord/application/views/contactus.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+		<meta charset="UTF-8"/>
+		<link rel="profile" href="http://gmpg.org/xfn/11"/>
+		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=yes">
+		<title>Contact Us &#8211; Zuhaus</title>
+		<?php $this->load->view("common/header-links.php");?>
+</head>
+<body class="page-template page-template-full-width page-template-full-width-php page page-id-117 mkd-core-1.0 mkdf-social-login-1.0 mkd-re-1.1.1 zuhaus child-child-ver-1.0.0 zuhaus-ver-1.1.1 mkdf-grid-1200 mkdf-sticky-header-on-scroll-up mkdf-dropdown-animate-height mkdf-header-standard mkdf-menu-area-shadow-disable mkdf-menu-area-in-grid-shadow-disable mkdf-menu-area-border-disable mkdf-menu-area-in-grid-border-disable mkdf-woocommerce-columns-3 mkdf-woo-normal-space mkdf-woo-pl-info-below-image mkdf-woo-single-thumb-below-image mkdf-woo-single-has-pretty-photo mkdf-default-mobile-header mkdf-sticky-up-mobile-header wpb-js-composer js-comp-ver-5.4.4 vc_responsive" itemscope itemtype="http://schema.org/WebPage">
+		<?php $this->load->view("common/header2");?>
+
+			        
+            <div class="mkdf-content" >
+                <div class="mkdf-content-inner">
+<div class="mkdf-full-width">
+    
+	<div class="mkdf-full-width-inner">
+        					<div class="mkdf-grid-row">
+				<div class="mkdf-page-content-holder mkdf-grid-col-12">
+					<div class="vc_row wpb_row vc_row-fluid" ><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner "><div class="wpb_wrapper"><div class="mkdf-google-map-holder">
+	<div class="mkdf-google-map" id="mkdf-map-1207674" data-addresses='["71-01 Metropolitan Ave, Middle Village, NY 11379, USA"]' data-custom-map-style=yes data-color-overlay=#393939 data-saturation=-100 data-lightness=0 data-zoom=13 data-pin=http://zuhaus.mikado-themes.com/wp-content/uploads/2017/10/google-pin-1.png data-unique-id=1207674 data-scroll-wheel=no data-height=440 ></div>
+			<div class="mkdf-google-map-overlay"></div>
+	</div>
+</div></div></div></div><div class="mkdf-row-grid-section-wrapper "  ><div class="mkdf-row-grid-section"><div class="vc_row wpb_row vc_row-fluid vc_custom_1510756575991" ><div class="wpb_column vc_column_container vc_col-sm-12 vc_col-lg-6"><div class="vc_column-inner "><div class="wpb_wrapper"><div class="mkdf-section-title-holder  mkdf-st-standard mkdf-st-title-center mkdf-st-normal-space " >
+	<div class="mkdf-st-inner">
+					<h2 class="mkdf-st-title" >
+				Send Us a Message			</h2>
+							<p class="mkdf-st-text" style="font-size: 18px">
+				We’d love to hear from you. Say us hello!			</p>
+			</div>
+</div><div class="vc_empty_space"   style="height: 9px" ><span class="vc_empty_space_inner"></span></div>
+<div role="form" class="wpcf7" id="wpcf7-f4-p117-o1" lang="en-US" dir="ltr">
+<div class="screen-reader-response"></div>
+<form action="/contact-us/#wpcf7-f4-p117-o1" method="post" class="wpcf7-form" novalidate="novalidate">
+<div style="display: none;">
+<input type="hidden" name="_wpcf7" value="4" />
+<input type="hidden" name="_wpcf7_version" value="4.9.1" />
+<input type="hidden" name="_wpcf7_locale" value="en_US" />
+<input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f4-p117-o1" />
+<input type="hidden" name="_wpcf7_container_post" value="117" />
+</div>
+<p><label> Name*<br />
+    <span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" /></span> </label></p>
+<p><label> Email*<br />
+    <span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" /></span> </label></p>
+<p><label> Phone<br />
+    <span class="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span> </label></p>
+<p><label> Message<br />
+    <span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false"></textarea></span> </label></p>
+<p><input type="submit" value="Send Message" class="wpcf7-form-control wpcf7-submit" /></p>
+<div class="wpcf7-response-output wpcf7-display-none"></div></form></div><div class="vc_empty_space"   style="height: 65px" ><span class="vc_empty_space_inner"></span></div>
+</div></div></div><div class="wpb_column vc_column_container vc_col-sm-12 vc_col-lg-6"><div class="vc_column-inner "><div class="wpb_wrapper"><div class="mkdf-elements-holder   mkdf-one-column  mkdf-responsive-mode-768 " ><div class="mkdf-eh-item    "  data-item-class="mkdf-eh-custom-4729" data-768-1024="0px 0px 0px 0%" data-680-768="0px 0px 0px 0%" data-680="0px 0px 0px 0%">
+	<div class="mkdf-eh-item-inner">
+		<div class="mkdf-eh-item-content mkdf-eh-custom-4729" style="padding: 0px 0px 0px 10%">
+			<div class="mkdf-section-title-holder  mkdf-st-standard mkdf-st-title-center mkdf-st-normal-space " >
+	<div class="mkdf-st-inner">
+					<h2 class="mkdf-st-title" >
+				About Our Company			</h2>
+							<p class="mkdf-st-text" style="font-size: 18px">
+				Hendrerit in vulputate velit esse molestie consequat.			</p>
+			</div>
+</div><div class="vc_empty_space"   style="height: 13px" ><span class="vc_empty_space_inner"></span></div>
+
+	<div class="wpb_text_column wpb_content_element " >
+		<div class="wpb_wrapper">
+			<p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent.</p>
+
+		</div>
+	</div>
+<div class="vc_empty_space"   style="height: 12px" ><span class="vc_empty_space_inner"></span></div>
+
+	<div class="wpb_text_column wpb_content_element " >
+		<div class="wpb_wrapper">
+			<p>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima.</p>
+
+		</div>
+	</div>
+<div class="mkdf-separator-holder clearfix  mkdf-separator-center mkdf-separator-full-width">
+	<div class="mkdf-separator" style="border-color: #e1e1e1;border-bottom-width: 1px;margin-top: 38px;margin-bottom: 40px"></div>
+</div>
+<div class="mkdf-icon-list-holder " >
+	<div class="mkdf-il-icon-holder">
+		<i class="mkdf-icon-simple-line-icon icon-location-pin " style="font-size: 13px"></i>	</div>
+	<p class="mkdf-il-text" style="padding-left: 20px">774 NE 84th St Miami, FL 33879</p>
+</div><div class="mkdf-icon-list-holder " >
+	<div class="mkdf-il-icon-holder">
+		<i class="mkdf-icon-simple-line-icon icon-call-end " style="font-size: 13px"></i>	</div>
+	<p class="mkdf-il-text" style="padding-left: 20px">Call us FREE +1 (800) 990 8877</p>
+</div><div class="mkdf-icon-list-holder " >
+	<div class="mkdf-il-icon-holder">
+		<i class="mkdf-icon-simple-line-icon icon-envelope " style="font-size: 13px"></i>	</div>
+	<p class="mkdf-il-text" style="padding-left: 20px">officeinfo@example.com</p>
+</div><div class="vc_empty_space"   style="height: 12px" ><span class="vc_empty_space_inner"></span></div>
+
+	<div class="wpb_text_column wpb_content_element " >
+		<div class="wpb_wrapper">
+			<p>Don’t forget to follow us on:</p>
+
+		</div>
+	</div>
+<div class="vc_empty_space"   style="height: 12px" ><span class="vc_empty_space_inner"></span></div>
+    <span class="mkdf-icon-shortcode mkdf-normal   " style="margin: 0px 15px 0px 0px" data-hover-color="#ffcc00" data-color="#010101">
+                    <a itemprop="url" class="" href="https://twitter.com/" target="_blank">
+                    <i class="mkdf-icon-font-awesome fa fa-twitter mkdf-icon-element" style="color: #010101;font-size:15px" ></i>                    </a>
+            </span>
+    <span class="mkdf-icon-shortcode mkdf-normal   " style="margin: 0px 15px 0px 0px" data-hover-color="#ffcc00" data-color="#010101">
+                    <a itemprop="url" class="" href="https://www.pinterest.com/" target="_blank">
+                    <i class="mkdf-icon-font-awesome fa fa-pinterest-p mkdf-icon-element" style="color: #010101;font-size:15px" ></i>                    </a>
+            </span>
+    <span class="mkdf-icon-shortcode mkdf-normal   " style="margin: 0px 15px 0px 0px" data-hover-color="#ffcc00" data-color="#010101">
+                    <a itemprop="url" class="" href="https://www.facebook.com/" target="_blank">
+                    <i class="mkdf-icon-font-awesome fa fa-facebook mkdf-icon-element" style="color: #010101;font-size:15px" ></i>                    </a>
+            </span>
+    <span class="mkdf-icon-shortcode mkdf-normal   " style="margin: 0px 15px 0px 0px" data-hover-color="#ffcc00" data-color="#010101">
+                    <a itemprop="url" class="" href="https://www.instagram.com" target="_self">
+                    <i class="mkdf-icon-font-awesome fa fa-instagram mkdf-icon-element" style="color: #010101;font-size:15px" ></i>                    </a>
+            </span>
+    <span class="mkdf-icon-shortcode mkdf-normal   " style="margin: 0px 15px 0px 0px" data-hover-color="#ffcc00" data-color="#010101">
+                    <a itemprop="url" class="" href="https://www.flickr.com/" target="_self">
+                    <i class="mkdf-icon-font-awesome fa fa-flickr mkdf-icon-element" style="color: #010101;font-size:15px" ></i>                    </a>
+            </span>
+<div class="vc_empty_space"   style="height: 65px" ><span class="vc_empty_space_inner"></span></div>
+		</div>
+	</div>
+</div></div></div></div></div></div></div></div>
+					</div>
+							</div>
+			</div>
+
+    </div>
+
+</div> <!-- close div.content_inner -->
+	</div>  <!-- close div.content -->
+	<?php $this->load->view("common/footer"); ?>
+    </div>
+  </div>
+<?php $this->load->view("common/footer-links"); ?>
+</body>
+</html>
+
+</body>
+</html>
